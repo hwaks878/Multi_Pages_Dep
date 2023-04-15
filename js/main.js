97586f9671c07a233736526bbfbcd2c4dbcb9007
hawks878 <65505682+hwaks878@users.noreply.github.com>
2023-04-15 06:17:40 -0700

    added the smoke screen overlay

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 function openClose() {
   //   let menuState = document.querySelector(".nav-item");
   //   menuState.classList.toggle("active");
+  let overLay = document.querySelector(".overlay");
   let menuState = document.querySelectorAll(".nav-item");
   for (let element of menuState) {
     element.classList.toggle("active");
   }
+  overLay.classList.toggle("active");
 }
 document.querySelector("#btn-menu").addEventListener("click", openClose);
