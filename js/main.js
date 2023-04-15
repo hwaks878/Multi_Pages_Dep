19d730b7db874325985e224fb8af1d57afe99d41
hawks878 <65505682+hwaks878@users.noreply.github.com>
2023-04-15 08:50:05 -0700

    add js for close menu

--- js/main.js
+++ js/main.js
@@ -2,10 +2,18 @@ function openClose() {
   //   let menuState = document.querySelector(".nav-item");
   //   menuState.classList.toggle("active");
   let overLay = document.querySelector(".overlay");
+  let iconChange = document.querySelector(".logo-hamburger");
   let menuState = document.querySelectorAll(".nav-item");
+  console.log(iconChange.innerText);
   for (let element of menuState) {
     element.classList.toggle("active");
   }
   overLay.classList.toggle("active");
+  if (overLay.classList.contains("active")) {
+    iconChange.innerText = "close";
+  } else {
+    iconChange.innerText = "menu";
+  }
 }
-document.querySelector("#btn-menu").addEventListener("click", openClose);
+const hamburgerBtn = document.querySelector("#btn-menu");
+hamburgerBtn.addEventListener("click", openClose);
